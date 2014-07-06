654265b8507de6f75df7f57defa405cb96d75d71
celineotter <celine.otter@gmail.com>
2014-07-05 22:48:38 -0700

    added git ignore

--- client/app/flashcards/flashcards.js
+++ client/app/flashcards/flashcards.js
@@ -4,4 +4,5 @@ angular.module('chinapp.flashcards', [])
 
 .controller('FlashcardsController', function ($scope, FlashcardData) {
   $scope.data = FlashcardData.vocabList;
+/*  $scope.hideSound = true;*/
 });
